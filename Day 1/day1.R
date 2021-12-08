960ae8465597eebaaa6690c787ab925bdf4fff0a
Jan Alexandersson <jan.alexandersson@kidbrooke.com>
2021-12-08 23:01:02 +0100

    Added part 2

--- Day 1/day1.R
+++ Day 1/day1.R
@@ -1,3 +1,9 @@
+# part 1
 input <- scan("input.txt")
-sum(diff(input, lag = 1) > 0)
+number_of_increases <- function(x) sum(diff(x, lag = 1) > 0, na.rm = TRUE)
+number_of_increases(input)
 #1475
+
+# part 2
+rolling_sum <- function(x) sapply(1:length(input), FUN = function(i) sum(input[i:(i+2)]))
+number_of_increases(rolling_sum(input))
